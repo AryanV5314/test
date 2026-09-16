c3dc0234a7f567f56ff6ea95335d7b687e8b41a2
AryanV5314 <aryanvaishnani5341@gmail.com>
2026-09-16 16:23:29 +0530

    Python Application check on Jenkins

--- app.py
+++
@@ -1,8 +0,0 @@
-this is for todays topic git commit --amend
-this is from git cherry-pick
-
-age=26
-new change for --no-ff commit of understanding commit in fast-forward merge
-new branch change
-This is from git revision 24 Hours
-new add line for git diff
